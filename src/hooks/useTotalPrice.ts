5d8787eefa60e8d6e992830420bd149f64cb5dbe
Viktor Vasylkovskyi <viktor@rely.io>
2025-03-19 14:51:19 +0000

    Add env variable to prod

--- src/hooks/useTotalPrice.ts
+++ src/hooks/useTotalPrice.ts
@@ -1,7 +1,8 @@
+
 import { GenericResponse, TotalPriceResponse } from "@/types/types";
 import { useFetch } from "./useFetch";
 
 
 export const useTotalPrice = (numberOfBags: number): GenericResponse<TotalPriceResponse> => {
-    return useFetch<TotalPriceResponse>(`http://localhost:3000/pricing?numberOfBags=${numberOfBags}`)
+    return useFetch<TotalPriceResponse>(`${process.env.SERVER_BASE_URL || 'http://localhost:3000'}/pricing?numberOfBags=${numberOfBags}`)
 }
